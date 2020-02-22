78599b50c49fb866c10b167e17b3f58615b22533
akhil kumar <52838239+Akhil-64@users.noreply.github.com>
2020-02-23 01:56:47 +0530

    Add files via upload

---
+++ bubble sort2.py
@@ -0,0 +1,18 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(0,len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+            
+
+
+arr=[64, 34, 25, 12, 22, 11, 90, 4, 5, 2 ,566]
+print("original array")
+for i in range(len(arr)):
+    print(arr[i],end=" ")
+
+bubbleSort(arr)
+print("\n")
+print("sorted array")
+for i in range(len(arr)):
+    print(arr[i],end=" ")
